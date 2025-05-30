b23162ab937235457451924f2e6669f5b310ab08
Sebastian Sauer <ssauer@posteo.de>
2025-05-30 12:15:51 +0200

    revision update

--- R-Code/funs.R
+++ R-Code/funs.R
@@ -21,7 +21,7 @@ compute_all_norms <- function(x, min_score = 0, max_score = max(x), by = .5){
       z = map_dbl(score, ~ convert_to_norm_value(.x, data_vec = x)),
       stanine = map_dbl(score, ~ convert_to_norm_value(.x, data_vec = x, mu = 5, sigma = 2)),
       T = map_dbl(score, ~ convert_to_norm_value(.x, data_vec = x, mu = 50, sigma = 10)),
-      perc_normal = map_dbl(z, pnorm)
+      percent_norm = map_dbl(z, pnorm)
     ) %>% 
     mutate(stanine = case_when(
       stanine > 9 ~ 9,
@@ -35,6 +35,7 @@ compute_all_norms <- function(x, min_score = 0, max_score = max(x), by = .5){
 
 
 
+
 plot_fmi_descriptives <- function(data, var) {
   
   d_prepped <- 
@@ -43,13 +44,11 @@ plot_fmi_descriptives <- function(data, var) {
     drop_na() %>% 
     group_by({{var}}) %>%
     describe_distribution(iqr = FALSE, range = TRUE, quartiles = TRUE) %>% 
-    select(Variable, Mean, SD, .group) %>% 
-    mutate(group = str_remove(.group, "^.+=")) %>% 
-    select(-.group)
+    select(Variable, Mean, SD, {{var}}) 
   
   d_prepped %>%  
     #pivot_longer(-c(Variable, group)) %>% 
-    ggplot(aes(x = group, color = Variable)) +
+    ggplot(aes(x = {{var}}, color = Variable)) +
     geom_errorbar(aes(ymin = Mean-(2*SD), ymax = Mean+(2*SD)), width = .1) +
     geom_point2(aes(y = Mean), alpha = .7, size = 2) +
     facet_wrap(~ Variable) +
@@ -76,6 +75,7 @@ describe_fmi_stats <- function(data, var){
     describe_distribution(iqr = FALSE, range = TRUE, quartiles = TRUE) %>% 
     mutate(Mean_01 = Mean/3) %>% 
     relocate(Mean_01, .after = Mean) %>% 
+    select(Variable, everything()) |> 
     arrange(Variable) %>% 
     display() 
 }
